a2f4eec3f314c9d193211521683b299cc41d561e
Sjors Wijsman <sjorswijsman@gmail.com>
2024-10-23 21:29:01 +0200

    seo & code improvements

---
+++ docs/_app/immutable/entry/start.DfhPBWRg.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DR98aw-5.js";export{t as start};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,48 +1,13 @@
 <script>
-	import { onMount } from 'svelte';
-
-	let mounted = false;
-
-	onMount(() => {
-		mounted = true;
-	});
+	import Hero from '$lib/components/Hero.svelte';
+	import Seo from 'sk-seo';
 </script>
 
-<main
-	class:unviewed={!mounted}
-	class="w-full h-[100vh] overflow-hidden flex flex-col justify-center items-center gap-2"
->
-	<h1 class="text-7xl font-title font-bold">
-		<span>Wesman</span><br />
-		<span>Solutions</span>
-	</h1>
-	<p class="first-line:text-gray-400">Tailor-made SaaS solutions for everyone</p>
-	<a
-		href="mailto:contact@wesmansolutions.com"
-		class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded mt-8"
-	>
-		Contact Us
-	</a>
-</main>
+<Seo
+	title="Wesman Solutions - Contact us!"
+	description="Tailor-made SaaS solutions for everyone"
+	siteName="Wesman Solutions"
+	imageURL="https://wesmansolutions.com/banner.png"
+/>
 
-<style>
-	main {
-		background: linear-gradient(to bottom, #1e1e25, #000);
-		transition: all 1s ease-out;
-	}
-
-	main.unviewed {
-		opacity: 0;
-	}
-
-	main > * {
-		display: inline-block;
-		transform: translateY(-1rem);
-		transition: all 1.2s ease-out;
-	}
-
-	main.unviewed > * {
-		transform: translateY(4rem);
-		opacity: 0;
-	}
-</style>
+<Hero />
